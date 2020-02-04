369a437c300ed68942931651df3c79e91ab0ce21
Derrick Black <dblack5@wwu.edu>
2020-02-03 17:33:27 -0800

    Finished DungeonMap.java (mostly)

--- src/DungeonMap.java
+++ src/DungeonMap.java
@@ -2,12 +2,45 @@
 public class DungeonMap {
     private Room[][] rooms;
     private Player player;
+    private final String WALL_MAP_POSITION = "*";
+    private final String EMPTY_MAP_POSITION = " ";
+    private char playerToken;
 
-    public DungeonMap (int rows, int columns){
 
+    public DungeonMap (int rows, int columns, Player player){
+        rooms = new Room[rows][columns];
+        this.player = player;
+        switch(player.getPlayerClass()) {
+            case "Warrior":
+                playerToken = 'W';
+            case "Thief":
+                playerToken = 'T';
+        }
     }
 
     public void print() {
 
+        for (int i = 0; i < rooms.length + 2; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        for (int i = 0; i < rooms.length; i++) {
+            System.out.print(WALL_MAP_POSITION);
+            for (int j = 0; j < rooms[i].length; j++) {
+                if (player.getY() == i + 1 && player.getX() == j + 1) {
+                    System.out.print(playerToken);
+                }
+                else {
+                    System.out.print(EMPTY_MAP_POSITION);
+                }
+            }
+            System.out.println(WALL_MAP_POSITION);
+        }
+
+        for (int i = 0; i < rooms.length + 2; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 }
