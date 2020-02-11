8cb2bea862ce246550fbc44c69f77ab931cabb48
Gavin-Roberts <robertg7@wwu.edu>
2020-02-10 16:12:23 -0800

    Filled the Methods of the Player Class, print statement for Monster damage still needed in Monster.java

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 
 public class Player {
     private int health;
@@ -5,49 +6,73 @@ public class Player {
     private int damage;
     private String playerClass;
     private double lootModifier;
-    private int[] coordinates;
+    private Point coordinates;
 
-    public void attack(Monster target) {
-
+    public Player(String playerClass){
+        gold = 0;
+        coordinates = new Point (0, 0);
+        switch(playerClass){
+            case "Warrior":
+                health = 100;
+                lootModifier = 1.0;
+                damage = 15;
+                break;
+            case "Thief":
+                health = 70;
+                lootModifier = 1.2;
+                damage = 10;
+                break;
+        }
     }
 
-    public Player(){}
+    public void attack(Monster target) {
+        target.onHit(damage);
+        System.out.println("The player attacks the " + target.getType() + " dealing " + damage + " damage!");
+    }
 
     public void onHit(int damage) {
+        health -= damage;
 
     }
 
     public void onHeal(int health) {
-
+        this.health += health;
+        System.out.println ("You found a healing potion that has restored your vigor, you gained" + health + "health. Your health is now " + this.health + " health!");
     }
 
     public void onLoot(int gold) {
-
+        this.gold += (gold * lootModifier);
+        System.out.println("You found " + gold + " gold! You now have " + this.gold + "gold!");
+        if (this.gold < 100) {
+            System.out.println ("Only " + (100 - this.gold) + " gold left!");
+        }
     }
 
-    public void setCoordinates(int x, int y) {
 
+
+    public void setCoordinates(int x, int y) {
+        coordinates.setLocation(x, y);
     }
 
     // accessor methods
 
     public int getHealth() {
-        return 0;
+        return health;
     }
 
     public int getGold() {
-        return 0;
+        return gold;
     }
 
     public String getPlayerClass() {
-        return null;
+        return playerClass;
     }
 
     public int getX() {
-        return 0;
+        return (int)coordinates.getX();
     }
 
     public int getY() {
-        return 0;
+        return (int)coordinates.getY();
     }
 }
